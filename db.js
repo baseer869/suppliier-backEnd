0adf571b544868b101c0dd899bbf9e365146a3ff
baseer869 <57503739+baseer869@users.noreply.github.com>
2022-04-25 10:31:53 -0700

    database config setup

--- db.js
+++ db.js
@@ -1,11 +1,24 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 module.exports.sequelize = Sequelize;
 module.exports.instance = function () {
+  let sequelize;
+  if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(
+      "kclpzjl4k26ckd6e",
+      "knloqgrr1hzptv94",
+      "w8x5bwxam1e56hqd",
+      {
+        host: "bv2rebwf6zzsv341.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        dialect: "mysql",
+      }
+    );
+  } else {
+    sequelize = new Sequelize("suppliier", "root", "root", {
+      host: "localhost",
+      dialect: "mysql",
+    });
+  }
 
-const sequelize = new Sequelize('suppliier', 'root', 'root',   {
-    host:'localhost',
-    dialect:'mysql'
-});
-return sequelize;
-}
+  return sequelize;
+};
