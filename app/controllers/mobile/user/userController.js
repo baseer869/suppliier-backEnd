8a256658c454959fd6022816077bc859657ae3a6
baseer869 <57503739+baseer869@users.noreply.github.com>
2022-07-03 00:44:45 -0700

    update user detail key

--- app/controllers/mobile/user/userController.js
+++ app/controllers/mobile/user/userController.js
@@ -1,68 +1,79 @@
-const models = require('../../../../database/sequelize/sequelize');
-var jwt = require('jsonwebtoken');
-const sendResponse = require('../../../utility/functon/sendResponse');
-const User = require('../../../../database/schemas/user');
+const models = require("../../../../database/sequelize/sequelize");
+var jwt = require("jsonwebtoken");
+const sendResponse = require("../../../utility/functon/sendResponse");
+const User = require("../../../../database/schemas/user");
 
 module.exports = {
+  fetchUserAddress: async (req, res, next) => {
+    try {
+      //    let {  } = body;
+      let findQuery = {
+        where: {
+          id: req.userId,
+          default: true,
+        },
+        attributes: [
+          "id",
+          "email",
+          "addressTitle",
+          "username",
+          "customerName",
+          "city",
+          "state",
+          "address",
+          "zip",
+          "phone",
+          "default",
+        ],
+      };
 
-    fetchUserAddress: async (req, res, next) => {
-        try {
-            //    let {  } = body;
-              let findQuery ={
-                  where :{ 
-                      id: req.userId, 
-                      default: true,
-                },
-             attributes:['id', 'email', 'addressTitle', 'username', 'customerName', 'city', 'state', 'address', 'zip', 'phone', 'default']
-              } 
+      let userAddress = await models.users.findOne(findQuery);
+      if (!userAddress) {
+        res.status(202).json({
+          status: 202,
+          message: "Address not found",
+          data: null,
+        });
+      }
+      res.status(200).json({
+        status: 200,
+        message: `Fetch successfully.`,
+        data: userAddress,
+      });
+    } catch (error) {
+      sendResponse.error(error);
+    }
+  },
 
-         let userAddress = await models.users.findOne(findQuery);
-         if(!userAddress){
-             res.status(202).json({
-                status: 202,
-                message: 'Address not found',
-                data: null
-            });
-         }     
-          res.status(200).json({
-            status: 200,
-            message: `Fetch successfully.`,
-            data: userAddress
-         });
-        } catch (error) {
-            sendResponse.error(error);
-        }
-    },
+  //
 
-
-
-    //
-
-    updateUserAddress: async (req, res, next) => {
-        try {
-               let bodyRequest =  req.body;
-               bodyRequest.default = true;
-              let findQuery ={
-                  where :{ 
-                      id: req.userId, 
-                },
-              } 
-
-         let [addressUpdated] = await models.users.update( bodyRequest, findQuery);
-         if(!addressUpdated){
-             res.status(404).json({
-                status: 404,
-                message: 'Failed to update address',
-                data: null
-            });
-         }     
-          res.status(200).json({
-            status: 200,
-            message: `Updated successfully.`,
-            data: addressUpdated
-         });
-        } catch (error) {
-            sendResponse.error(error);
-        }
-    },
-}
+  updateUserAddress: async (req, res, next) => {
+    try {
+      let bodyRequest = req.body;
+      bodyRequest.default = true;
+      let findQuery = {
+        where: {
+          id: req.userId,
+        },
+      };
+      if (req.body.username) {
+        bodyRequest.username = req.body.username;
+      }
+      let [addressUpdated] = await models.users.update(bodyRequest, findQuery);
+      if (!addressUpdated) {
+        res.status(404).json({
+          status: 404,
+          message: "Failed to update address",
+          data: null,
+        });
+      }
+      res.status(200).json({
+        status: 200,
+        message: `Updated successfully.`,
+        data: addressUpdated,
+      });
+    } catch (error) {
+      sendResponse.error(error);
+    }
+  },
+};
