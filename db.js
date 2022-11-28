b9d1546a1ce616458c0dec26b15339e32734985f
baseer ahmed <baseer.ahmed@veroke.com>
2022-11-28 10:41:00 +0500

    database changes

--- db.js
+++ db.js
@@ -4,17 +4,15 @@ module.exports.sequelize = Sequelize;
 module.exports.instance = function () {
   let sequelize;
   if (process.env.ENV === 'localhost') {
-    sequelize = new Sequelize( "epiz_33044828_staging",
-    "epiz_33044828",
-    "kuPSP8eAvRfKmv",
+    sequelize = new Sequelize( "resello-staging",
+    "admin",
+    "admin123",
     {
-      host: "sql205.epizy.com",
+      host: "resello-staging.c89akl5uhanc.ap-south-1.rds.amazonaws.com",
       dialect: "mysql",
     });
-    console.log('sequelize', sequelize);
    
   } else if(process.env.ENV === 'staging') {
-    console.log('staging');
     sequelize = new Sequelize(
       "resello-staging",
       "admin",
